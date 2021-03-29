bce1ca9373149b9903d4b4ea2c2b98cdfc1495bd
ts <ts@goldcarrot.ru>
2021-03-29 20:50:07 +0300

    pagination trait

---
+++ src/Http/Traits/PaginationTrait.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created date 26.03.2021
+ * @author Sergey Tyrgola <ts@goldcarrot.ru>
+ */
+
+namespace GoldcarrotLaravel\Http\Traits;
+
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\JsonResponse;
+
+trait PaginationTrait
+{
+    public function jsonPagination(LengthAwarePaginator $paginator, $items = null): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'data' =>  $items ?: $paginator->items(),
+            'pagination' => [
+                'currentPage' => $paginator->currentPage(),
+                'lastPage' => $paginator->lastPage(),
+                'firstPageUrl' => $paginator->url(1),
+                'lastPageUrl' => $paginator->url($paginator->lastPage()),
+                'nextPageUrl' => $paginator->nextPageUrl(),
+                'prevPageUrl' => $paginator->previousPageUrl(),
+                'perPage' => $paginator->perPage(),
+                'total' => $paginator->total(),
+            ]
+        ]);
+    }
+}
